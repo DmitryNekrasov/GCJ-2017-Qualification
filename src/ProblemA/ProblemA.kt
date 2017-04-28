0375589d15035805ecf4bcfa749f65eb8d88b298
Dmitry Nekrasov <scannorone@gmail.com>
2017-04-28 11:21:13 +0700

    Problem A. Oversized Pancake Flipper

--- src/ProblemA/ProblemA.kt
+++ src/ProblemA/ProblemA.kt
@@ -3,7 +3,7 @@ package ProblemA
 import java.io.*
 
 fun main(args: Array<String>) {
-    val br = BufferedReader(FileReader(File("input.txt")))
+    val br = BufferedReader(FileReader(File("A-large-practice.in")))
     val pw = PrintWriter(File("output.txt"))
 
     solve(br, pw)
@@ -13,5 +13,23 @@ fun main(args: Array<String>) {
 }
 
 fun solve(br: BufferedReader, pw: PrintWriter) {
-    println("Hello!")
+    val testCount = br.readLine().toInt()
+    for (test in 1..testCount) {
+        val (patternStr, kStr) = br.readLine().split(" ")
+        val pattern = patternStr.map { it == '+' }.toBooleanArray()
+        val k = kStr.toInt()
+
+        var ans = 0
+
+        for (i in 0..pattern.size - k) {
+            if (!pattern[i]) {
+                ans++
+                for (j in i..i + k - 1) {
+                    pattern[j] = !pattern[j]
+                }
+            }
+        }
+
+        pw.println("Case #$test: ${if (false !in pattern) ans.toString() else "IMPOSSIBLE"}")
+    }
 }
